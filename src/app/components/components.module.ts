20a823c0d57e36cf978f3354161d723307a8cbbc
Isaac Bustamante <izaack.97@gmail.com>
2020-08-07 18:26:41 -0700

    Pipe DonSanitizer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
 import { IonicModule } from '@ionic/angular';
+import { DomSanitizerPipe } from '../pipes/dom-sanitizer.pipe';
+import { PipesModule } from '../pipes/pipes.module';
 
 
 
@@ -16,7 +18,8 @@ import { IonicModule } from '@ionic/angular';
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
