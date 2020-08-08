a140c104a4344461073ff1babfaf7e53523265cc
Isaac Bustamante <izaack.97@gmail.com>
2020-08-07 18:45:06 -0700

    Added infinity scroll in home

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -13,10 +13,23 @@ export class Tab1Page implements OnInit {
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {
+    this.siguientes();
+  }
+
+  siguientes(event?) {
     this.postsService.getPosts()
       .subscribe(resp => {
         console.log(resp);
         this.posts.push(...resp.posts);
+
+        if (event) { // Si existe el elemento vuelves a cargar los posts
+          event.target.complete();
+
+          // Lo desactivamos cuando no haya nada publicaciones
+          if (resp.posts.length === 0) {
+            event.target.disabled = true;
+          }
+        }
       });
   }
 
